b1571b85373b4715b32ba20965732b6293f223b3
Prad23 <pradyu23@gmail.com>
2019-04-29 23:24:44 -0700

    Updated with accuweather api
    Why is it necessary? (Bug fix, feature, improvements?)
    improvement
    
    How does the change address the issue?
    With updated api, we can loop through cities to get weather for all.
    
    What side effects does this change have?
    None

--- app.py
+++ app.py
@@ -1,41 +1,45 @@
 import sys
 sys.path.append('./modules')
 import httpclient, logging, errors, operator as op, json, config_loader
-from flask import Flask
+from flask import Flask, request
 
 Format = '%(asctime)-15s:%(name)s:%(levelname)s--> %(message)s'
 logging.basicConfig (format=Format, filename="weather-service.log", level="INFO", filemode="a")
 log = logging.getLogger(__name__)
 app = Flask(__name__)
 
-def _weather_url(weather,city,state):
-    return weather['weather.host']+weather['weather.token']+weather['weather.query']+"/"+state+"/"+city+".json"    
-
 @app.route('/')
 def index():
     return 'Index Page'
 
-@app.route('/weather/<string:city>/<string:state>',methods=['GET'])
-def main(city,state):
+@app.route('/weather',methods=['GET','POST'])
+def main():
     """
     Gets weather response for given location.
 
     Response returned is full json body. 
     """
     try:
-        log.info("Weather service is being invoked.")
-        log.info("Getting weather for %s,%s" %(city,state) )
-        configLoader = config_loader.ConfigurationLoader()
-        configLoader.set_configsection('Weather_API')
-        weather = configLoader.getconfig()
-        log.info("Get weather for location %s,%s " % (city,state))
-        weather_url = _weather_url(weather,city,state)
-        log.info("Weather url for current location is: %s " % weather_url)
-        client = httpclient.HttpClient()
-        client.set_url(weather_url)
-        w_resp=client.get_request()
-        log.info("Weather response: %s" %w_resp)
-        return json.dumps(w_resp)
+        loop_var = request.args.getlist('city')
+        log.info("Number of arguments in Weather service: %s" % len(loop_var))
+        response = []
+        for city in loop_var:
+            log.info("Weather service is being invoked")
+            configLoader = config_loader.ConfigurationLoader()
+            configLoader.set_configsection('Weather_API')
+            weather = configLoader.getconfig()
+            log.info("Get weather for location %s" % (city))
+            weather_url = weather['weather.host']+weather['weather.resource']+"/"+str(city)
+            log.info("Weather url for current location is: %s " % weather_url)
+            client = httpclient.HttpClient()
+            client.set_url(weather_url)
+            queryString = {"apikey": weather['weather.token'],"details":"false"}
+            client.set_params(queryString)
+            log.info("Weather url after construction %s,%s " %(client.get_url(),client.get_params()))
+            w_resp=client.get_request()
+            log.info("Weather response: %s" %w_resp)
+            response.append(w_resp)
+        return json.dumps(response)
     except errors.Error as err:
         log.error("Distance response could not be retrieved: %s" % err)
 
